c1fb47a50ef85eb1ee0c4d3cb2464d6f31087daf
taqimanjay <151718265+taqimanjay@users.noreply.github.com>
2023-12-21 18:36:05 +0700

    Add files via upload

---
+++ Modul5-main/main.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Content_Type: application/json; charset=UTF-8");
+
+include "Routes/ProductRoutes.php";
+
+use app\Routes\ProductRoutes;
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$productRoute = new ProductRoutes();
+$productRoute->handle($method, $path);
